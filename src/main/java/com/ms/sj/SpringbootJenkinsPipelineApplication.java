eaf012b77204601da87184b1c8819b2933513db4
maadhu2811 <madhujamaadhu2711@gmail.com>
2022-03-27 16:55:44 +0530

    frst commit

---
+++ src/main/java/com/ms/sj/SpringbootJenkinsPipelineApplication.java
@@ -0,0 +1,24 @@
+package com.ms.sj;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsPipelineApplication {
+
+	public static Logger logger=LoggerFactory.getLogger(SpringbootJenkinsPipelineApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Pipeline Application started");
+	}
+	public static void main(String[] args) {
+		logger.info(" Pipeline Application executed");
+		SpringApplication.run(SpringbootJenkinsPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/sj/SpringbootJenkinsPipelineApplicationTests.java
@@ -0,0 +1,21 @@
+package com.ms.sj;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsPipelineApplicationTests {
+	public static Logger logger=LoggerFactory.getLogger(SpringbootJenkinsPipelineApplicationTests.class);
+
+
+	@Test
+	void contextLoads() {
+		logger.info("Pipeline Test case executing..");
+		assertEquals(true, true );
+	}
+
+}
